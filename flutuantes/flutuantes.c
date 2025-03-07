9031d7217cb9597c110ab18b0c252e56e29b26e3
programacao-ctrl <werykgithub@gmail.com>
2025-03-07 14:14:05 +0000

    variação

---
+++ flutuantes/flutuantes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+  int main(){
+
+  float x = 5.5;
+  float y = 2.2;
+  float soma = x + y;
+  float diferenca = x - y;
+  float produto = x * y;
+  float quociente = x / y;  //divisão de ponto flutuante
+
+
+  printf("a soma: %.2f\n", soma);
+  printf("a diferença: %.2f\n", diferenca);
+  printf("o produto: %.2f\n", produto);
+  printf("o quociente:  %.2f\n", quociente);
+  }
---
+++ flutuantes/inversão.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+  int main(){
+
+  int a = 10;
+  float b = 3.5;
+  float resultado = a + b; //'a' é convertido em float implicitamente
+
+
+
+   printf(" o resultado é:  %.2f", resultado);
+
+
+
+
+    return 0;
+  }
