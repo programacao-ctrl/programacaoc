2619bd50f132f1772f3571d1abcca139a801e42f
programacao-ctrl <werykgithub@gmail.com>
2025-03-07 14:32:31 +0000

    inversões explicitas

---
+++ flutuantes/inversãõexpli.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+  int main(){
+
+  int a = 10;
+  int b = 3;
+  float quociente = (float) a / b; //'a' é explicitamente convertido em float
+
+   printf("quociente: %.2f", quociente);
+
+
+
+ return 0;
+
+
+}
